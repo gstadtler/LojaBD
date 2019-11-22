1c769d96148f9c695a2829cde194e3b974a574b9
gstadtler <ghs2@cin.ufpe.br>
2019-11-22 10:12:45 -0300

    Criando o repositório do projeto

---
+++ connection.py
@@ -0,0 +1,16 @@
+import psycopg2
+#sql = "insert into cidade values (default,'São Paulo,'SP')"
+#cur.execute(sql)
+#con.commit()
+
+con = psycopg2.connect(host='localhost', database='LojaBD', user='postgres', password='29272212')
+cur = con.cursor()
+sql = 'create table teste (id serial primary key, nome varchar(100), sobrenome varchar(2))'
+cur.execute(sql)
+"""
+cur.execute('select * from teste')
+recset = cur.fetchall()
+for rec in recset:
+    print(rec)
+con.close()
+"""
