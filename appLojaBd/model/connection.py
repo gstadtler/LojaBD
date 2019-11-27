bd2a1ada98dc4ec9f11849069f47cfb6350803c6
gstadtler <ghs2@cin.ufpe.br>
2019-11-26 23:30:08 -0300

    atualização da conexão com o banco

--- appLojaBd/model/connection.py
+++ appLojaBd/model/connection.py
@@ -3,20 +3,26 @@ import psycopg2
 
 class Connection():
 
-    def __init__(self, host='localhost', database='LojaBD', user='postgres', password='postgres123'):
-        self.conn = psycopg2.connect(database=db, user=user)
+    def __init__(self, host='localhost', db='LojaBD', user='postgres', password='postgres123'):
+        self.conn = psycopg2.connect(host=host, database=db, user=user, password=password)
         self.cur = self.conn.cursor()
 
     def query(self, query):
         self.cur.execute(query)
-
+    
+    def queryResult(self):
+        self.result = self.cur.fetchall()
+        for row in self.result:
+            print(row)
+            
     def close(self):
         self.cur.close()
         self.conn.close()
 
 
 db = Connection()
-db.query("Select * from cliente")
+db.query('Select * from cliente')
+db.queryResult()
 db.close()
 
 # con.commit()
